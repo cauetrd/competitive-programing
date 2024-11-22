0c1d655977dd3066e97c97e40ddc045b2f2650af
cauetrindade <150065608+cauetrindade@users.noreply.github.com>
2024-11-21 23:07:13 -0300

    init

---
+++ algorithms/template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define vi vector<int>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define vll vector<long long>
+
+void solve() {
+
+
+
+}
+
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+
+
+    return 0;
+}
